90ea24515cfbff51dd553f4c1b8572f8ce559136
pacosonTang <pacoson@163.com>
2018-08-30 23:18:50 +0800

    spring rest programming style, @PathVariable, ant path and requestmapping request ways like url, method or headers.

---
+++ springmvc01/src/main/java/com/swjtu/springmvc/handler/SpringMvcTest.java
@@ -0,0 +1,89 @@
+package com.swjtu.springmvc.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @RequestMapping 既可以修饰类也可以修饰方法 
+ */ 
+@RequestMapping("/springmvc")
+@Controller
+public class SpringMvcTest {
+	private final static String SUCCESS = "success";
+	
+	/**
+	 * rest 风格url。
+	 * 以 crud为例：
+	 * 新增： /order POST
+	 * 修改：/order/1 PUT
+	 * 获取：/order/1 GET
+	 * 删除：/order/1  DELETE
+	 * 
+	 * 如何发送 PUT 请求 和 DELETE 请求呢？
+	 * 1、需要配置 filter hiddenHttpMethodFilter
+	 * 2、需要发送 POST 请求
+	 * 3、需要在发送 POST 请求时， 携带一个 隐藏域 name="_method"，
+	 * 值 value="DELETE" 或 value="PUT"
+	 * 
+	 *  在 springmvc 中的 目标方法中如何得到 id 呢？
+	 */
+	
+	/**
+	 * Rest 风格编程
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value="/testRest/{id}", method=RequestMethod.GET)
+	public String testRest(@PathVariable("id") Integer id) {
+		System.out.println("testRest(), id = " + id);
+		return SUCCESS;
+	}
+	
+	/**
+	 * @PathVariable 可以来映射 URL 中的占位符到目标方法的参数中.
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping("/testPathVariable/{id}")
+	public String testPathVariable(@PathVariable("id") Integer id) {
+		System.out.println("testPathVariable(), id = " + id);
+		return SUCCESS;
+	}
+	
+	@RequestMapping("/testAntPath/**/abc")
+	public String testAntPath() {
+		System.out.println("testAntPath");
+		return SUCCESS;
+	}
+	/**
+	 * 使用参数和请求头进行映射，不常用，仅了解 
+	 * @return
+	 */
+	@RequestMapping(
+			value="/testParamsAndHeaders"
+			, params={"username", "age!=10"}
+			, headers={"Accept-Language: zh-CN,zh;q=0.8"}
+	)
+	public String testParamsAndHeaders() {
+		System.out.println("SpringMvcTest.testParamsAndHeaders() ");
+		return SUCCESS;
+	}
+	
+	/**
+	 * 使用 method 属性来指定请求方式 
+	 * @return
+	 */
+	@RequestMapping(value="/testMethod", method=RequestMethod.POST)
+	public String testMethod() {
+		System.out.println("SpringMvcTest.testMethod() ");
+		return SUCCESS;
+	}
+	
+	@RequestMapping("/testRequestMapping")
+	public String hello() {
+		System.out.println("SpringMvcTest.hello() ");
+		return SUCCESS;
+	}
+}
--- springmvc01/src/main/webapp/index.jsp
+++ springmvc01/src/main/webapp/index.jsp
@@ -1,12 +1,45 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+<%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
-	<a href="helloworld">hello world</a>
+	
+	<br/><br/>
+	<!-- 更新操作 -->
+	<form action="springmvc/testRest/3" method="post">
+		<input type="hidden" name="_method" value="PUT">
+		<input type="submit" value="test rest put"/>
+	</form>
+	<br/><br/>
+	
+	<!-- 删除操作 -->
+	<form action="springmvc/testRest/2" method="post">
+		<input type="hidden" name="_method" value="DELETE">
+		<input type="submit" value="test rest delete"/>
+	</form>
+	<br/><br/>
+	
+	<a href="/springmvc/testRest/1">test rest get</a>	
+	<br/><br/>
+	
+	<a href="springmvc/testPathVariable/120">testPathVariable</a>
+	<br/>
+	
+	<a href="springmvc/testAntPath/1/2/abc">testAntPath</a>
+	<br/>
+	
+	<a href="springmvc/testParamsAndHeaders?username=1&age=11">testParamsAndHeaders</a>
+	<br/>
+	
+	<form action="springmvc/testMethod" method="post">
+		<input type="submit" value="提交"/>
+	</form> 
+	<a href="springmvc/testMethod">testMethod</a><br/>
+	<a href="springmvc/testRequestMapping">testRequestMapping</a><br/>
+	<a href="helloworld">hello world</a> <br/>
 </body>
 </html>
