b483746ffbee28c0321e8a0bd9eaf97972570905
pacosonTang <pacoson@163.com>
2018-09-21 23:45:15 +0800

    springmvc ioc vs spring ioc.

---
+++ springmvc03/src/main/java/com/swjtu/springmvc/service/UserService.java
@@ -0,0 +1,13 @@
+package com.swjtu.springmvc.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+public class UserService {
+	
+	public UserService() {
+		System.out.println("UserService Constructor.");
+	}
+	
+}
--- springmvc03/src/main/java/com/swjtu/springmvc/test/MySpringMvcTest.java
+++ springmvc03/src/main/java/com/swjtu/springmvc/test/MySpringMvcTest.java
@@ -1,15 +1,26 @@
 package com.swjtu.springmvc.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.swjtu.springmvc.service.UserService;
 
 @Controller
 @RequestMapping("/springmvc")
 public class MySpringMvcTest {
 
+	@Autowired
+	private UserService userService;
+	
+	public MySpringMvcTest() {
+		System.out.println("MySpringMvcTest  constructor."); 
+	}
+	
 	@RequestMapping(value="/helloworld")
 	public String helloworld() {
 		
+		System.out.println("userService = " + userService);
 		System.out.println("success!! "); 
 		return "success";
 	}
